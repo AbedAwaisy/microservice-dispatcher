44e74861ffee78a1eb6460624819e9056b3bbeba
AbedAwaisy <abed.awaisy.a@gmail.com>
2023-09-13 20:34:08 +0300

    fixed priv key path + fixed severity

--- backend/notificationServices/errorProcessor.js
+++ backend/notificationServices/errorProcessor.js
@@ -8,24 +8,21 @@ const saveLog = require('./saveLog'); // Import function to save logs in the dat
 async function processError(errorDetails) {
   const { severity, phoneNumber, email, iotIp, userName, ...otherDetails } = errorDetails;
 
-  ///data for jira
-  const jiraBaseUrl = 'https://jirapr124.atlassian.net'; // Corrected base URL
-  const username = 'rahmaazaiza2002@gmail.com';
-  const apiToken = 'ATATT3xFfGF0jou8BuyT0H-_GAaurlyFBldCWlRcMboinBQVaZuRC_spu3DICIgagyCsREC4BzGqar8HDERMQyMexYyhKHJJ-hUyKxOwI6d9cpx7FyWlSTrgjl-V0nnQezgqm7dxKvL0QkofgE6eTO1qZK2Du7j_qis3BZvQVauQ5SvXTryinH4=8AF2CF52'; // Replace with your API token
-  const projectKey = 'KAN';
+  
   // Process error based on severity
   switch (severity) {
     case 'High':
-     
+      await phoneCall(phoneNumber,"hi Abed, how are you");
+      await sendSMS(phoneNumber,"hi Abed, how are you");
+      await sendJira(jiraBaseUrl, username,apiToken,projectKey,"helooooooo");
+
       break;
     case 'Medium':
       await sendIoTNotification(iotIp, `High-severity error: ${otherDetails}`);
       await sendJira(jiraBaseUrl, username,apiToken,projectKey,"helooooooo");
-      await phoneCall(phoneNumber,"hi alaa, how are you");
-      await sendSMS(phoneNumber,"hi alaa, how are you");
       break;
     case 'Low':
-      // Handle low-severity errors
+      await sendJira(jiraBaseUrl, username,apiToken,projectKey,"helooooooo");
       break;
     default:
       // Handle other cases
--- backend/notificationServices/phoneCall.js
+++ backend/notificationServices/phoneCall.js
@@ -1,16 +1,20 @@
 const { Vonage } = require('@vonage/server-sdk')
 const { NCCOBuilder, Talk, OutboundCallWithNCCO } = require('@vonage/voice')
+const path = require('path');
+
+const privateKeyPath = path.join(__dirname, 'private.key');
 
 const vonage = new Vonage({
   apiKey: 'f02de533', // Replace with your actual API key
   apiSecret: 'dosk6L7wty9i2RNC', // Replace with your actual API secret
   applicationId: '3d2dc681-3cb6-43a8-8dcf-2425cdd943a5', // Replace with your actual application ID
-  privateKey: 'D:\\project\\courseFullStack\\final\\microservice-dispatcher\\backend\\notificationServices\\private.key', // Replace with the actual path to your private key file
+  privateKey: privateKeyPath, // Replace with the actual path to your private key file
 });
 
 
 async function phoneCall(phoneNumber,text) {
   try {
+    console.log(privateKeyPath);
     const builder = new NCCOBuilder();
     builder.addAction(new Talk(`${text}`));
     const resp = await vonage.voice.createOutboundCall(
